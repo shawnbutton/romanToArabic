ac206a35ab5c628abb09baa495039732fb65a1ca
shawn <shawnbutton@gmail.com>
2021-11-13 17:00:03 -0500

    convert to jest each parameterized tests

--- tests/roman.test.js
+++ tests/roman.test.js
@@ -3,11 +3,11 @@
 const sut = require('../src/roman')
 
 describe('Roman to Arabic Number Convertor', () => {
-  it('converts I', () => {
-    expect(sut.romanToArabic('I')).toEqual(1)
+  it.each([
+    ['I', 1],
+    ['II', 2]
+  ])('converts %s to %i', (given, expected) => {
+    expect(sut.romanToArabic(given)).toEqual(expected)
   })
 
-  it('converts II', () => {
-    expect(sut.romanToArabic('II')).toEqual(2)
-  })
 })
